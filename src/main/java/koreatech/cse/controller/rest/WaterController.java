3183369032df2c4648ffa1a023ac924903da26ba
LimDongGyu <limdg5335@gmail.com>
2018-12-11 22:20:33 +0900

    Update: weatherURL api endpoint
    Update: weatherURL api endpoint
    
    before :  String strUrl = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib";      /* 초단기 실황*/
    
    after :   String strUrl = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData"; /* 동네예보조회 -> numOfRows=9 로 설정하면 됨*/

--- src/main/java/koreatech/cse/controller/rest/WaterController.java
+++ src/main/java/koreatech/cse/controller/rest/WaterController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.InputStreamReader;
+import java.lang.reflect.ParameterizedType;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 @RestController
 public class WaterController {
@@ -26,6 +28,7 @@ public class WaterController {
         urlBuilder.append("&" + URLEncoder.encode("s_page","UTF-8") + "=" + URLEncoder.encode("0", "UTF-8")); /*조회 시작 지점*/
         urlBuilder.append("&" + URLEncoder.encode("s_list","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 번에 조회될 최대 row 갯수*/
         urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*XML/JSON 여부*/
+
 //        urlBuilder.append("&" + URLEncoder.encode("mnrlsp_nm","UTF-8") + "=" + URLEncoder.encode("88약수터", "UTF-8")); /*약수터명*/
 //        urlBuilder.append("&" + URLEncoder.encode("rdnmadr","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*소재지도로명주소*/
 //        urlBuilder.append("&" + URLEncoder.encode("lnmadr","UTF-8") + "=" + URLEncoder.encode("경기도 동두천시 생연동 산70번지", "UTF-8")); /*소재지지번주소*/
@@ -40,7 +43,29 @@ public class WaterController {
 //        urlBuilder.append("&" + URLEncoder.encode("institution_nm","UTF-8") + "=" + URLEncoder.encode("경기도 동두천시청", "UTF-8")); /*관리기관명*/
 //        urlBuilder.append("&" + URLEncoder.encode("reference_date","UTF-8") + "=" + URLEncoder.encode("2018-06-30", "UTF-8")); /*데이터기준일자*/
 //        urlBuilder.append("&" + URLEncoder.encode("instt_code","UTF-8") + "=" + URLEncoder.encode("3740000", "UTF-8")); /*제공기관코드*/
+
         urlBuilder.append("&" + URLEncoder.encode("instt_nm","UTF-8") + "=" + URLEncoder.encode("경기도 수원시", "UTF-8")); /*제공기관명*/
+
+
+
+        /*---------------------------------------------------------------
+          |  * 제공받는 내용
+          |  - 위치 정보(좌표? 지역명?) 구체적일 필요가 있다.
+          |  - 각 지역에 나오는 약수터가 많다면 다 비교할 필요 있음.
+          |    = 적합도를 기준으로 자르고 데이터를 저장한 뒤에 제공(자체배열? DB?)
+          |  - nx, ny (위도, 경도) -> weatherController
+          ---------------------------------------------------------------*/
+
+
+        /*--------------------------------------------------
+          |  * 제공해야하는 내용
+          |  - 약수터명          mnrlsp_nm
+          |  - 소재지도로명주소   rdnmadr
+          |  - 소재지지번주소     lnmadr
+          |  - 수질검사일자       gltwtr_inspct_date
+          |  - 수질검사결과구분   gltwtr_inspct_result_type
+          |  - 부적합항목        impropt_iem
+          --------------------------------------------------*/
 
 
         System.out.println(urlBuilder);
@@ -71,5 +96,34 @@ public class WaterController {
         rd.close();
         conn.disconnect();
         System.out.println(sb.toString());
+
+
+
+        //context debug
+        int cnt = 0;
+
+        String sbResult = sb.toString();
+        String[] sbList = sbResult.split(",");
+
+
+        for(String item : sbList){
+            System.out.println(cnt + " : " + item);
+            cnt++;
+        }
+
+
+//        //특정단어(부분)만 자르기
+//        System.out.println("특정단어 부분만 자르기");
+//        String target = "약수터명\":\"";
+//        int target_num = sbResult.indexOf(target);
+//        String targetResult = sbResult.substring(target_num, (sbResult.substring(target_num).indexOf("소") + target_num));
+//        System.out.println(targetResult);
+//
+//        String target2 = "소재지지번주소";
+//        int target_num2 = sbResult.indexOf(target2);
+//        String targetResult2 = sbResult.substring(target_num2, (sbResult.substring(target_num2).indexOf("위") + target_num2));
+//        System.out.println(targetResult2);
+
+
     }
 }
--- src/main/java/koreatech/cse/controller/rest/WeatherController.java
+++ src/main/java/koreatech/cse/controller/rest/WeatherController.java
@@ -35,16 +35,79 @@ public class WeatherController {
 
 //        String str = URLEncoder.encode()
 
-        String strUrl = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib";
+//        String strUrl = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib";      /* 초단기 실황*/
+//        String strUrl = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData"; /* 초단기예보조회 */
+        String strUrl = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData"; /* 동네예보조회 -> numOfRows=9 로 설정하면 됨*/
+
         strUrl += "?serviceKey=" + weather_service_key;
-        strUrl += "&base_date=20181210";
-        strUrl += "&base_time=2330";
-        strUrl += "&nx=55";
-        strUrl += "&ny=127";
+        strUrl += "&base_date=20181210";                                 /*발표일자*/
+        strUrl += "&base_time=2330";                                     /*발표시각*/
+        strUrl += "&nx=55";                                              /*예보지점 X 좌표*/
+        strUrl += "&ny=127";                                             /*예보지점 Y 좌표*/
         strUrl += "&pageNo=1";
-        strUrl += "&numOfRows=3";
+        strUrl += "&numOfRows=9";
         strUrl += "&_type=json";
 
+
+        /*--------------------------------------------------
+          |  * 제공해야하는 내용
+          |  - 실황 값          obsrValue
+          |  - 자료구분코드      category
+          |  - 예측일자         fcstDate
+          |  - 예측시간         fcstTime
+          |  - 예보 값         fcstValue
+          --------------------------------------------------*/
+
+        /*---------------------------------------------------
+          |                코드값 정보 (동네예보)
+          |--------------------------------------------------
+          | 항목값 |         항목 명         | 단위
+          |--------------------------------------------------
+          |  POP  |         강수확률        | %
+          |  PTY  |         강수형태        | 코드값
+          |  SKY  |         하늘상태        | 코드값
+          |  T3H  |        3시간 기온       | 도
+          |  R06  |       6시간 강수량      |  범주(1mm)
+          |  REH  |          습도          |  %
+          |  WSD  |          풍속          |  1
+          --------------------------------------------------*/
+
+        // 이 값들은 fcstValue에 담겨 있다.
+
+
+        /*---------------------------------------------------
+          | 하늘상태 : 맑음(1), 구름조금(2), 구름많음(3), 흐림(4)
+          | 강수상태 : 없음(0), 비(1), 비/눈(2)=진눈깨비, 눈(3)
+          |--------------------------------------------------
+          |                     강수량 범주
+          |--------------------------------------------------
+          |         범주        |   문자열 표시 | GRIB 저장값
+          |--------------------------------------------------
+          |      0.1mm 미만     | 0mm 또는 없음 |  0
+          | 0.1mm 이상  1mm 미만 |   1mm 미만   |  1
+          |  1mm 이상  5mm 미만  |    1~4mm    |  5
+          |  5mm 이상  10mm 미만 |    5~9mm    |  10
+          | 10mm 이상  20mm 미만 |   10~19mm   |  20
+          | 20mm 이상  40mm 미만 |   20~39mm   |  40
+          | 40mm 이상  70mm 미만 |   40~69mm   |  70
+          |       70mm 이상     |   70mm 이상  |  100
+          --------------------------------------------------*/
+
+        /*---------------------------------------------------
+          |                      풍속
+          |--------------------------------------------------
+          |   풍속 구간   |  통보문  |           비고
+          |--------------------------------------------------
+          |    4미만     |    약    | 연기 흐름에 따라 풍향 감지 가능
+          | 4이상 9미만   |  약간강  | 안면에 감촉을 느끼면서 나뭇잎이 조금 흔들림
+          | 9이상 14미만  |    강    | 나무가지와 깃발이 가볍게 흔들림
+          |   14이상     |   매우강  | 먼지가 일고, 작은 나무 전체가 흔들림
+          ---------------------------------------------------*/
+
+
+
+
+        System.out.println(strUrl);
 
         URL url = new URL(strUrl);
         HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
